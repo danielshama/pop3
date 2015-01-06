462b49fa21f8d9c2e3f057ab1764c2a48910d0f2
Or <orbenda1905@gmail.com>
2015-01-06 21:31:41 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitproject
+//
+//  Created by Or Ben David on 1/6/15.
+//  Copyright (c) 2015 Or Ben David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
