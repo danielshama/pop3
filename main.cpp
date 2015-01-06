8cfea13c0fcdf6ad1616b1b3cd41ef4292289ddc
Daniel Shamama <daniel.shama@gmail.com>
2015-01-06 20:19:58 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pop3
+//
+//  Created by Daniel Shamama on 1/6/15.
+//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
