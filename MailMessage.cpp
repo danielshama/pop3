3f92d95b3e1b2bf38d960bfb26053e93e3e14c80
Daniel Shamama <daniel.shama@gmail.com>
2015-01-06 22:42:12 +0200

    copy changed

--- MailMessage.cpp
+++ MailMessage.cpp
@@ -3,7 +3,7 @@
 //  pop3
 //
 //  Created by Daniel Shamama on 1/6/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
+//  Copyright (c) 2015 Daniel Shamama  & Or Ben David. All rights reserved.
 //
 
 #include "MailMessage.h"
