72381bf4b3faf68394c760795f7a475b2fe17c22
Or <orbenda1905@gmail.com>
2015-01-07 15:20:39 +0200

    a

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //  pop3
 //
 //  Created by Daniel Shamama on 1/6/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
+//  Copyright 2015 Daniel Shamama. All rights reserved.
 //
 
 #include "Includes.h"
