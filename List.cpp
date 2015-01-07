ef46edfaec73b98b306ca16674db7676765f778b
Daniel Shamama <daniel.shama@gmail.com>
2015-01-07 17:06:55 +0200

    new folders

--- List.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  List.cpp
-//  pop3
-//
-//  Created by Daniel Shamama on 1/7/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
-//
-
-#include "List.h"
--- Pop3Adaptor.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Pop3Adaptor.cpp
-//  pop3
-//
-//  Created by Daniel Shamama on 1/6/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
-//
-
-#include "Pop3Adaptor.h"
