022d16444db62a749af0e529a042c794483697dd
Daniel Shamama <daniel.shama@gmail.com>
2015-01-07 19:46:58 +0200

    Re-create generic list container

--- DataModels/List.cpp
+++ DataModels/List.cpp
@@ -3,7 +3,35 @@
 //  pop3
 //
 //  Created by Daniel Shamama on 1/7/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
+//  Copyright (c) 2015 Daniel Shamama & Or Ben David. All rights reserved.
 //
 
 #include "List.h"
+
+template <class T>
+List<T>::List(): count(0)
+{
+    array = new T[N];
+}
+
+
+template <class T>
+void List<T>::add(T newObj)
+{
+    if(count % N == 0 && count != 0){
+        T newArr = new T[count+N];
+        for(int i=0 ; i<count ; i++){
+            newArr[i]=array[i];
+        }
+        delete[] array;
+        array = newArr;
+    }else array[count] = newObj;
+    
+    count++;
+}
+
+template <class T>
+List<T>::~List()
+{
+    delete[] array;
+}
--- DataModels/List.h
+++ DataModels/List.h
@@ -3,19 +3,30 @@
 //  pop3
 //
 //  Created by Daniel Shamama on 1/7/15.
-//  Copyright (c) 2015 Daniel Shamama. All rights reserved.
+//  Copyright (c) 2015 Daniel Shamama & Or Ben David. All rights reserved.
 //
 
 #ifndef __pop3__List__
 #define __pop3__List__
 
-#include <stdio.h>
-
+#include "Includes.h"
+#define N 5
 template <class T>
-class List{
+class List
+{
 private:
-    int x;
+    
+    T *array;
+    int count;
+    
 public:
+    
+    List();
+    void add(T);
+    void del(T);
+    int getCount();
+    ~List();
+    
 };
 
 #endif /* defined(__pop3__List__) */
