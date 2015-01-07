9f96b749b37dc48768ccf9a3f24ca1403820dc44
Daniel Shamama <daniel.shama@gmail.com>
2015-01-07 10:07:25 +0200

    change main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,4 @@
-//
-//  main.cpp
-<<<<<<< HEAD
-//  gitproject
-//
-//  Created by Or Ben David on 1/6/15.
-//  Copyright (c) 2015 Or Ben David. All rights reserved.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-=======
 //  pop3
 //
 //  Created by Daniel Shamama on 1/6/15.
