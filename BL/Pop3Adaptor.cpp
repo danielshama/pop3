13d97dfe1a9bc9ab69abed33c839309c8661ef3e
Daniel Shamama <daniel.shama@gmail.com>
2015-01-10 12:21:39 +0200

    /*

--- BL/Pop3Adaptor.cpp
+++ BL/Pop3Adaptor.cpp
@@ -8,7 +8,7 @@
 
 #include "Pop3Adaptor.h"
 
-
+/*
 Pop3Adaptor::Pop3Adaptor(string dataFile){
     
 }
@@ -36,3 +36,4 @@ const char* Pop3Adaptor::RSET(){
 const char* Pop3Adaptor::QUIT(){
     
 }
+*/
