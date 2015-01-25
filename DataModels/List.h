552d105ab7366779eb02143872bb7e73c9a3f6bc
Or <orbenda1905@gmail.com>
2015-01-25 11:31:31 +0200

    nothing

--- DataModels/List.h
+++ DataModels/List.h
@@ -24,7 +24,7 @@ public:
     
     List();               //constructor
     bool empty();               //returns true if the list is empty, false otherwise
-    int size();                 //returns the number of elements in the list
+    int size();                 //returns the number of nodes
     void insert(T d); //inserts a node before the current node
     void clearMarks(int);
     void markForRemove(int);
